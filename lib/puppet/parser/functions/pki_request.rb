dbe83fcd9c87ff50b5dc2abc62e805f2c684035c
Chris Barker <cbarker@puppetlabs.com>
2013-04-12 13:23:38 -0700

    updated function now works

--- lib/puppet/parser/functions/pki_request.rb
+++ lib/puppet/parser/functions/pki_request.rb
@@ -1,6 +1,6 @@
 module Puppet::Parser::Functions
   newfunction(:pki_request) do |args|
     hostname = args[0]
-    Puppet::Util::Execution.execute "/bin/echo #{hostname} >> /tmp/test_hostname"
+    Puppet::Util.execute "/bin/echo #{hostname} >> /tmp/test_hostname"
   end
 end
